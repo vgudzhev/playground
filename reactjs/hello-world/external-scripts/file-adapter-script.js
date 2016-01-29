3497f135f31ed43884e1dd2a0a2a884c8293268c
vgudzhev <vasil.gudzhev@inteqrity.com>
2016-01-29 14:58:43 +0200

    Add external-script modification

--- reactjs/hello-world/external-scripts/file-adapter-script.js
+++ reactjs/hello-world/external-scripts/file-adapter-script.js
@@ -1,3 +1,6 @@
+
+// Simple pure-React component so we don't have to remember
+// Bootstrap's classes
 var BootstrapButton = React.createClass({
   render: function() {
     return (
@@ -26,22 +29,97 @@ var BootstrapModal = React.createClass({
     $(this.refs.root).modal('show');
   },
   render: function() {
+    var confirmButton = null;
+    var cancelButton = null;
+
+    if (this.props.confirm) {
+      confirmButton = (
+        <BootstrapButton
+          onClick={this.handleConfirm}
+          className="btn-primary">
+          {this.props.confirm}
+        </BootstrapButton>
+      );
+    }
+    if (this.props.cancel) {
+      cancelButton = (
+        <BootstrapButton onClick={this.handleCancel} className="btn-default">
+          {this.props.cancel}
+        </BootstrapButton>
+      );
+    }
+
     return (
       <div className="modal fade" ref="root">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h3>This is a header</h3>
+              <button
+                type="button"
+                className="close"
+                onClick={this.handleCancel}>
+                &times;
+              </button>
+              <h3>{this.props.title}</h3>
             </div>
             <div className="modal-body">
-              <h3>This is a body</h3>
+              {this.props.children}
             </div>
             <div className="modal-footer">
-              <h3>Thank you Captain Obvious</h3>
+              {cancelButton}
+              {confirmButton}
             </div>
           </div>
         </div>
       </div>
     );
+  },
+  handleCancel: function() {
+    if (this.props.onCancel) {
+      this.props.onCancel();
+    }
+  },
+  handleConfirm: function() {
+    if (this.props.onConfirm) {
+      this.props.onConfirm();
+    }
   }
 });
+
+var Example = React.createClass({
+  handleCancel: function() {
+    if (confirm('Are you sure you want to cancel?')) {
+      this.refs.modal.close();
+    }
+  },
+  render: function() {
+    var modal = null;
+    modal = (
+      <BootstrapModal
+        ref="modal"
+        confirm="OK"
+        cancel="Cancel"
+        onCancel={this.handleCancel}
+        onConfirm={this.closeModal}
+        title="Hello, Bootstrap!">
+          This is a React component powered by jQuery and Bootstrap!
+      </BootstrapModal>
+    );
+    return (
+      <div className="example">
+        {modal}
+        <BootstrapButton onClick={this.openModal} className="btn-default">
+          Open modal
+        </BootstrapButton>
+      </div>
+    );
+  },
+  openModal: function() {
+    this.refs.modal.open();
+  },
+  closeModal: function() {
+    this.refs.modal.close();
+  }
+});
+
+ReactDOM.render(<Example />, document.getElementById('componentContainer'));
