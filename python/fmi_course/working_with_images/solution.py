c33810c16ec9fb29206d7d94f62387b213a298ad
vgudzhev <vgudzhev@gmail.com>
2016-11-26 17:51:42 +0200

    Create structure for solved tasks

---
+++ python/fmi_course/working_with_images/solution.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+
+
+def rotate_left(image):
+    return [[image[row][col] for row in range(len(image))]
+            for col in range(len(image[0])-1, -1, -1)]
+
+
+def rotate_right(image):
+    return [[image[row][col] for row in range(len(image)-1, -1, -1)]
+            for col in range(0, len(image[0])-1)]
+
+
+def invert_pixel(pixel):
+    return tuple(255 - color for color in pixel)
+
+
+def invert(image):
+    return [[invert_pixel(pixel) for pixel in row] for row in image]
+
+
+def lighten_pixel(coef, pixel):
+    return tuple(90 + int(coef * (255 - color)) for color in pixel)
+
+
+def lighten(image, c):
+    return [[lighten_pixel(c, pixel) for pixel in row] for row in image]
+
+
+def darken_pixel(coef, pixel):
+    return tuple(color - int(coef * (color - 0)) for color in pixel)
+
+
+def darken(image, c):
+    return [[darken_pixel(c, pixel) for pixel in row] for row in image]
+
+
+def create_histogram(image):
+    histogram = {
+        'red': defaultdict(int),
+        'green': defaultdict(int),
+        'blue': defaultdict(int),
+    }
+
+    for row in image:
+        for pixel in row:
+            histogram['red'][pixel[0]] += 1
+            histogram['green'][pixel[1]] += 1
+            histogram['blue'][pixel[2]] += 1
+    return histogram
